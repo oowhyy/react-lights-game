fb9b152afde9e2d3668fb85727cb5805e18b8804
oowhyy <116504896+oowhyy@users.noreply.github.com>
2022-10-23 19:34:18 +0300

    add local project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App-main">
+          <div className="title"><h1>Title</h1></div>
+          <div className="description"><h3>description</h3></div>
+            <div className="bulb">
+              <div className="inner-bulb">
+
+              </div>
+          </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
